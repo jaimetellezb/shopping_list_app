7561910b0b3ba614c7847867db17b5d42ce55115
jaimetellezb <jaimetellez410@gmail.com>
2026-05-25 21:41:11 -0500

    Revert edge-to-edge (engine-level concern)

--- android/app/src/main/kotlin/com/jaimetellezb/shopping_list/MainActivity.kt
+++ android/app/src/main/kotlin/com/jaimetellezb/shopping_list/MainActivity.kt
@@ -1,12 +1,5 @@
 package com.jaimetellezb.shopping_list
 
-import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import io.flutter.embedding.android.FlutterActivity
 
-class MainActivity : FlutterActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        enableEdgeToEdge()
-        super.onCreate(savedInstanceState)
-    }
-}
+class MainActivity : FlutterActivity()
